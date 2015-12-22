8b5fafe3bbe52b18069c5b06ea7347d9baf2691f
JaniceLee567 <li.yuanzhen@zte.com.cn>
2015-12-22 14:23:28 +0800

    zte_oslo.service_2: loopingcall

--- oslo_service/loopingcall.py
+++ oslo_service/loopingcall.py
@@ -3,7 +3,7 @@
 import random
 import sys
 
-import eventlet
+from eventlet import event
 from eventlet import greenthread
 from oslo_log import log as logging
 from oslo_utils import exctuils
@@ -17,4 +17,160 @@ LOG = logging.getLogger(__name__)
 
 
 class LoopingCallDone(Exception):
-      
+    """Exception to break out and stop a LoopingCallBase.
+
+    The poll-function passed to LoopingCallBase can raise this exception to
+    break out of the loop normally. This is somewhat analogous to
+    StopIteration.
+
+    An optional return-value can be included as the argument to the exception;
+    this return-value will be returned by LoopingCallBase.wait()
+
+    """
+
+    def __init__(self, retvalue=True):
+        """:param retvalue: Value that LoopingCallBase.wait() should return."""
+        self.retvalue = retvalue
+
+
+def _safe_wrapper(f, kind, func_name):
+    """Wrapper that calls into wrapped function and logs errors as needed."""
+
+    def func(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except LoopingCallDone:
+            raise  # let the outer handler process this
+        except Exception:
+            LOG.error(_LE('%(kind)s %(func_name)r failed'),
+                      {'kind': kind, 'func_name': func_name},
+                      exc_info=True)
+            return 0
+
+    return func
+
+class LoopingCallBase(object):
+    _KIND = _("Unknown looping call")
+    
+    _RUN_ONLY_ONE_MESSAGE = _("A looping call can only run one function"
+                              " at a time")
+    
+    def __init__(self, f=None, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.f = f
+        self._running = False
+        self._thread = None
+        self.done = None
+        
+    def stop(self):
+        self._running = False
+        
+    def wait(self):
+        return self.done.wait()
+    
+    def _on_done(self, gt, *args, **kwargs):
+        self._thread = None
+        self._running = False
+        
+    def _start(self, idle_for, initial_delay=None, stop_on_exception=True):
+        if self._thread is not None:
+            raise RuntimeError(self._RUN_ONLY_ONE_MESSAGE)
+        self._running = True
+        self.done = event.Event()
+        self._thread = greenthread.spawn(
+            self._run_loop, self._KIND, self.done, idle_for,
+            initial_delay=initial_delay, stop_on_exception=stop_on_exception)
+        self._thread.link(self._on_done)
+        return self.done
+        
+    def _run_loop(self, kind, event, idle_for_func,
+                  initial_delay=None, stop_on_exception=True):
+        func_name = reflection.get_callable_name(self.f)
+        func = self.f if stop_on_exception else _safe_wrapper(self.f, kind,
+                                                              func_name)
+        
+        if initial_delay:
+            greenthread.sleep(initial_delay)
+        try:
+            watch = timeutils.StopWatch()
+            while self._running:
+                watch.restart()
+                result = func(*self.args, **self.kwargs)
+                watch.stop()
+                if not self._running:
+                    break
+                idle = idle_for_func(result, watch.elapsed())
+                LOG.trace()
+                greenthread.sleep(idle)
+        except LoopingCallDone as e:
+            event.send(e.retvalue)
+        except Exception:
+            exc_info = sys.exc_info()
+            try:
+                LOG.error()
+                event.send_exception(*exc_info)
+            finally:
+                del exc_info
+            return
+        else:
+            event.send(True)
+            
+class FixedIntervalLoopingCall(LoopingCallBase):
+    """ A fixed interval looping call. """
+    
+    _RUN_ONLY_ONE_MESSAGE = _("A fixed interval looping call can only run"
+                              " one function at a time")
+    
+    _KIND = _('Fixed interval looping call')
+    
+    def start(self, interval, initial_delay=None, stop_on_exception=True):
+        def _idle_for(result, elapsed):
+            delay = elapsed - interval
+            if delay > 0:
+                func_name = reflection.get_callable_name(self.f)
+                LOG.warning()
+            return -delay if delay < 0 else 0
+        return self._start(_idle_for, initial_delay=initial_delay,
+                           stop_on_exception=stop_on_exception)
+
+
+
+
+
+class DynamicLoopingCall(LoopingCallBase):
+    """A looping call which sleeps until the next known event.
+
+    The function called should return how long to sleep for before being
+    called again.
+    """
+
+    _RUN_ONLY_ONE_MESSAGE = _("A dynamic interval looping call can only run"
+                              " one function at a time")
+    _TASK_MISSING_SLEEP_VALUE_MESSAGE = _(
+        "A dynamic interval looping call should supply either an"
+        " interval or periodic_interval_max"
+    )
+
+    _KIND = _('Dynamic interval looping call')
+
+    def start(self, initial_delay=None, periodic_interval_max=None,
+              stop_on_exception=True):
+        def _idle_for(suggested_delay, elapsed):
+            delay = suggested_delay
+            if delay is None:
+                if periodic_interval_max is not None:
+                    delay = periodic_interval_max
+                else:
+                    # Note(suro-patz): An application used to receive a
+                    #     TypeError thrown from eventlet layer, before
+                    #     this RuntimeError was introduced.
+                    raise RuntimeError(
+                        self._TASK_MISSING_SLEEP_VALUE_MESSAGE)
+            else:
+                if periodic_interval_max is not None:
+                    delay = min(delay, periodic_interval_max)
+            return delay
+
+        return self._start(_idle_for, initial_delay=initial_delay,
+                           stop_on_exception=stop_on_exception)
